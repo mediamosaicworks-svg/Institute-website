ab0f58116a16f249d38a8f44992c735fc559953c
mediamosaicworks-svg <mediamosaicworks@gmail.com>
2026-06-21 19:01:14 +0530

    Fix contact and Google Business links

--- site-data.js
+++ site-data.js
@@ -8,10 +8,10 @@ window.ONLINE_SITE_DATA = {
     instagram: "https://www.instagram.com/mosaic_works_institute?igsh=ZXp6ZXF4czk2ZWtn",
     facebook: "https://www.facebook.com/share/1VC8kk6S8E/?mibextid=wwXIfr",
     website: "https://mosaicworksinstitute.in",
-    address: "Shop No. 16, Second Floor, Agarwal Market, Mahaveer Chowk, South Civil Lines, Muzaffarnagar, Uttar Pradesh 251001ा",
+    address: "Shop No. 16, Second Floor, Agarwal Market, Mahaveer Chowk, South Civil Lines, Muzaffarnagar, Uttar Pradesh 251001",
     locality: "Muzaffarnagar",
     region: "Uttar Pradesh",
     pincode: "251001",
-    googleBusiness: "address: "Shop No. 16, Second Floor, Agarwal Market, Mahaveer Chowk, South Civil Lines, Muzaffarnagar, Uttar Pradesh 251001","
+    googleBusiness: "https://www.google.com/maps/search/?api=1&query=Mosaic+Works+Institute+Muzaffarnagar"
   }
 };
