5da702ba87a881065fa0fbabfb57272a1f76d729
mediamosaicworks-svg <mediamosaicworks@gmail.com>
2026-06-21 18:40:31 +0530

    Update site-data.js

--- site-data.js
+++ site-data.js
@@ -1,2 +1,17 @@
 // Online admin saves published website content in this file.
-window.ONLINE_SITE_DATA = null;
+window.ONLINE_SITE_DATA = {
+  settings: {
+    name: "Mosaic Works Institute",
+    logo: "assets/mosaic-works-logo.png",
+    phone: "917217216035",
+    email: "mediamosaicworks@gmail.com",
+    instagram: "https://www.instagram.com/mosaic_works_institute?igsh=ZXp6ZXF4czk2ZWtn",
+    facebook: "https://www.facebook.com/share/1VC8kk6S8E/?mibextid=wwXIfr",
+    website: "https://mosaicworksinstitute.in",
+    address: "Shop No. 16, Second Floor, Agarwal Market, Mahaveer Chowk, South Civil Lines, Muzaffarnagar, Uttar Pradesh 251001ा",
+    locality: "Muzaffarnagar",
+    region: "Uttar Pradesh",
+    pincode: "251001",
+    googleBusiness: "https://www.google.com/maps/dir/Mosaic+works+institute,+Shop+No.+16,+Second+Floor,+Agarwal+Market,+Mahaveer+Chowk,+S+Civil+Lines,+Muzaffarnagar,+Uttar+Pradesh+251001/Mosaic+works+institute,+Shop+No.+16,+Second+Floor,+Agarwal+Market,+Mahaveer+Chowk,+S+Civil+Lines,+Muzaffarnagar,+Uttar+Pradesh+251001/@29.458432,77.7125888,14z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x390c1bf0b87e58fb:0x520eb3cadab7222b!2m2!1d77.7044594!2d29.465405!1m5!1m1!1s0x390c1bf0b87e58fb:0x520eb3cadab7222b!2m2!1d77.7044594!2d29.465405?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D"
+  }
+};
