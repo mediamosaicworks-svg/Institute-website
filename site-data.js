b7d6271e484de1f72d872216cb70022d62c5442c
mediamosaicworks-svg <mediamosaicworks@gmail.com>
2026-06-21 20:12:59 +0530

    Use uploaded transparent institute logo

--- site-data.js
+++ site-data.js
@@ -2,7 +2,7 @@
 window.ONLINE_SITE_DATA = {
   "settings": {
     "name": "Mosaic Works Institute",
-    "logo": "assets/mosaic-works-logo.png",
+    "logo": "assets/mosaic-works-logo-white.png",
     "phone": "917217216035",
     "email": "mediamosaicworks@gmail.com",
     "instagram": "https://www.instagram.com/mosaic_works_institute?igsh=ZXp6ZXF4czk2ZWtn",
